5275ac8ac14b53c0c0d90660481471f21780f013
tonyaalin <zdzo@yahoo.com.tw>
2016-04-15 09:27:12 +0800

    Update scratchX_http.js

--- scratchX_http.js
+++ scratchX_http.js
@@ -15,28 +15,30 @@
               dataType: 'jsonp',
               success: function( blynk_data ) {
                   // Got the data - parse it and return the temperature
-                  callback(blynk_data);
+                  temperature = blynk_data;
+                  callback(temperature);
               }
         });
     };
 
-     //ext.get_blynk = function(location, callback) {
+     ext.get_temp = function(location, callback) {
         // Make an AJAX call to the Open Weather Maps API
-     //   $.ajax({
-     //         url: 'http://cloud.blynk.cc:8080/' + location,
-     //         dataType: 'jsonp',
-     //         success: function( blynk_data ) {
-     //             // Got the data - parse it and return the temperature
-     //             temperature = blynk_data['main']['temp'];
-     //             callback(temperature);
-      //        }
-     //   });
-     // };
+        $.ajax({
+              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
+              dataType: 'jsonp',
+              success: function( weather_data ) {
+                  // Got the data - parse it and return the temperature
+                  temperature = weather_data['main']['temp'];
+                  callback(temperature);
+              }
+        });
+    };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['R', 'blynk Auth Token %s', 'get_blynk', 'xxxxxxxxxxxxxxxxxx/pin/V10'],
+            ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
         ]
     };
 
